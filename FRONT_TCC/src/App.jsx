78a2400fc8acae8c45b7c7ce3d07ef7d02ded5a7
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-05-23 22:39:11 -0300

    mudei o nome da tela inicial para VisaoGeral

--- FRONT_TCC/src/App.jsx
+++ FRONT_TCC/src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import Tela_login from './login/Tela_login'
 import Cadastrar from './login/Cadastrar'
-import Tela_inicial from './Paginas/Tela_inicial'
+import Tela_inicial from './Paginas/VisaoGeral'
 import '/global.css'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Tela_login/>}/>
           <Route path="/Cadastrar" element={<Cadastrar/>}/>
-          <Route path="/Principal" element={<Tela_inicial/>}/>
+          <Route path="/VisaoGeral" element={<Tela_inicial/>}/>
         </Routes>
     </>
   )
--- FRONT_TCC/src/Paginas/VisaoGeral.jsx
+++ FRONT_TCC/src/Paginas/VisaoGeral.jsx
@@ -1,7 +1,7 @@
 import MenuSuperior from "./MenuSuperior"
 import MenuLateral from "./MenuLateral"
 
-function Tela_inicial(){
+function VisaoGeral(){
     
     const tela = {
         backgroundColor: "#FFFFF", 
@@ -12,14 +12,8 @@ function Tela_inicial(){
             <div>
                 <MenuSuperior></MenuSuperior>
                 <MenuLateral></MenuLateral>
-                {/* <Icone 
-                    icon = {<FontAwesomeIcon icon="fa-solid fa-pills" style={{color: "#9ca3af",}}/>}
-                    href =""
-                 >
-                    Medicamentos
-                </Icone> */}
             </div>
         </div>
     )
 }
-export default Tela_inicial
+export default VisaoGeral
