a8f523b92780cf41060b7cad246f5375ad9d41a3
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-05-25 21:12:38 -0300

    comecei a criar a tela de medicamentos

---
+++ FRONT_TCC/src/Paginas/Medicamentos.jsx
@@ -0,0 +1,12 @@
+import TituloPagina from "./Componentes/TituloPagina";
+
+function Medicamentos(){
+    return(
+        <div>
+            <TituloPagina titulo="Medicamentos" descricao="Gerencie todos os medicamentos que você utiliza.">
+                Novo Medicamento
+            </TituloPagina>
+        </div>
+    );
+}
+export default Medicamentos;
