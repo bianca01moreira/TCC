0914988e02f4bb3722d0f83e58d4ec21248cae4b
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-05-25 22:29:49 -0300

    deixei as rotas prontas

--- FRONT_TCC/src/App.jsx
+++ FRONT_TCC/src/App.jsx
@@ -3,7 +3,9 @@ import Tela_login from './login/Tela_login'
 import Cadastrar from './login/Cadastrar'
 import Tela_inicial from './Paginas/VisaoGeral'
 import Medicamentos from './Paginas/Medicamentos'
+import Layout from './Paginas/Componentes/Layout'
 import '/global.css'
+import VisaoGeral from './Paginas/VisaoGeral'
 
 function App() {
   return (
@@ -11,8 +13,18 @@ function App() {
       <Routes>
           <Route path="/" element={<Tela_login/>}/>
           <Route path="/Cadastrar" element={<Cadastrar/>}/>
-          <Route path="/VisaoGeral" element={<Tela_inicial/>}/>
-          <Route path="/Medicamentos" element={<Medicamentos/>}></Route>
+
+         <Route element={<Layout/>}>
+              <Route path="/VisaoGeral" element={<VisaoGeral/>}/>
+              <Route path="/Medicamentos" element={<Medicamentos/>}/>
+              {/* <Route path="/Relatorios" element={<Relatorios/>}/>
+              <Route path="/Historico" element={<Historico/>}/>
+              <Route path="/Receitas" element={<Receitas/>}/>
+              <Route path="/Profissionais" element={<Profissionais/>}/>
+              <Route path="/Dependentes" element={<Dependentes/>}/>
+              <Route path="/Configuracoes" element={<Configuracoes/>}/>
+               */}
+          </Route>
         </Routes>
     </>
   )
