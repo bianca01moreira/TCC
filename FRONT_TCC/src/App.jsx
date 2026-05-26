e436daca31353a104f81a36d3d2884ade86e40c4
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-05-25 21:10:51 -0300

    criei a rota para a tela de medicamentos

--- FRONT_TCC/src/App.jsx
+++ FRONT_TCC/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import Tela_login from './login/Tela_login'
 import Cadastrar from './login/Cadastrar'
 import Tela_inicial from './Paginas/VisaoGeral'
+import Medicamentos from './Paginas/Medicamentos'
 import '/global.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Tela_login/>}/>
           <Route path="/Cadastrar" element={<Cadastrar/>}/>
           <Route path="/VisaoGeral" element={<Tela_inicial/>}/>
+          <Route path="/Medicamentos" element={<Medicamentos/>}></Route>
         </Routes>
     </>
   )
